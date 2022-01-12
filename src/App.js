8bd30d74ea0470e9c8f0c4e814020cbbdd061f23
winniewyl <90722768+winniewyl@users.noreply.github.com>
2022-01-12 14:49:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import sketch from './sketch';
-import p5 from 'p5';
+//imr
+import React from 'react';
+//imrd
 
-const TEXT = 'Happy Birthday Trisha';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Clock from './components/Clock';
+
+import Cake from './components/Cake';
+import Animation from './components/Animation';
+import Gifts from './components/Gifts';
 
 const App = () => {
-  const canvasRef = React.useRef(null)
+  return <div className="container"></div>;
+};
 
-  React.useEffect(() => {
-    const canvasDivElement = canvasRef.current;
+ReactDOM.render(
+  <>
+    <React.StrictMode>
+      <Animation />
+    </React.StrictMode>
 
-    // NOTE: If you change TEXT value to something else, you'll need to make changes in sketch.js as well to make it work
-    // See my comments in sketch.js
-    new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
-  })
+    {/* <Header /> */}
+    <Clock />
 
-  return (
-    <div className="main">
-      <div ref={canvasRef} />
-    </div >
-  )
-}
+    <Cake />
+    <Gifts />
+  </>,
+  document.getElementById('root')
+);
 
 export default App;
+
+
+
+
+
+// import React from 'react';
+// import './App.css';
+// import sketch from './sketch';
+// import p5 from 'p5';
+
+// const TEXT = 'Happy Birthday Trisha';
+
+// const App = () => {
+//   const canvasRef = React.useRef(null)
+
+//   React.useEffect(() => {
+//     const canvasDivElement = canvasRef.current;
+
+//     // NOTE: If you change TEXT value to something else, you'll need to make changes in sketch.js as well to make it work
+//     // See my comments in sketch.js
+//     new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
+//   })
+
+//   return (
+//     <div className="main">
+//       <div ref={canvasRef} />
+//     </div >
+//   )
+// }
+
+// export default App;
